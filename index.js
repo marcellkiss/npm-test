148190b21d7fcd692780750e8ac56ad0c4b1c091
marcellkiss <kissmarcell88@gmail.com>
2017-07-09 20:58:54 +0200

    add basic typing - working version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import * as THREEx from 'ar.js';
 
-console.log(THREEx);
+console.log('This is the so called AR.js object, ready for use', THREEx);
+
+//let arClickability = new THREEx.ARClickability(...
+
+// arClickability.onResize()
